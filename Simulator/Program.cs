977c8aba07ec0da44df8368b312fa4f57d20e96a
Proxy <pjeromaster@gmail.com>
2024-06-13 13:15:12 +0200

    moved eventhandlers into named methods

--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -1,54 +1,29 @@
-﻿using System.Timers;
+﻿using Timer = System.Timers.Timer;
 
 namespace Simulator;
-internal class Program
+internal static class Program
 {
     static void Main(string[] args)
     {
-        Console.ForegroundColor = ConsoleColor.White;
-        Console.CursorVisible = false;
+        Console.ForegroundColor = ConsoleColor.White; Console.CursorVisible = false;
+
+        Timer timer = new(interval:1000);
+        int StepCount = 0;
 
         string csvPath = (args.Length > 0) ? args[0] : FindCSVPath();
-
         PatriotModel patriot = new(new CSVFileRadar(csvPath), new OddFoeIdentifier(), new FiringUnit(0.8f));
-
-        Console.WriteLine("Initiated Patriot Model");
-        System.Timers.Timer timer = new(interval:1000);
-        int StepCount = 0;
+        patriot.FriendFoeIdentifier.OnHostileDetection += OnHostileDetection;
+        patriot.FiringUnit.OnMissileFired += OnMissileFired;
 
         timer.Elapsed += ( (object? sender, ElapsedEventArgs e) =>
         {
             StepCount++;
-            Console.CursorLeft = 0;
             Console.ForegroundColor = ConsoleColor.White;
-            Console.Write($"\n{StepCount,3} | ");
-            Console.CursorLeft = 35;
-            Console.Write('|');
-            Console.CursorLeft = 56;
-            Console.Write('|');
+            Console.CursorLeft = 0;  Console.Write($"\n{StepCount,3} | ");
+            Console.CursorLeft = 35; Console.Write('|');
+            Console.CursorLeft = 56; Console.Write('|');
             patriot.Update();
         } );
-
-        patriot.FriendFoeIdentifier.OnHostileDetection += ( (object? sender, EventArgs e) =>
-        {
-            Console.CursorLeft = 7;
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.Write("Hostile Entity Identified");
-        });
-        patriot.FiringUnit.OnMissileFired += ( (object? sender, MissileFiredEventArgs e) =>
-        {
-            Console.CursorLeft = 37;
-            if(e.TargetNeutralised)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write("Target Neutralized");
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write("Target Operational");
-            }
-        });
 
         timer.Start();
 
@@ -57,6 +32,31 @@ internal class Program
         Console.WriteLine("\nSimulation is finished");
     }
 
+    private static void OnMissileFired(object? sender, MissileFiredEventArgs e)
+    {
+        Console.CursorLeft = 37;
+        if(e.TargetNeutralised)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Target Neutralized");
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Target Operational");
+        }
+    }
+
+    private static void OnHostileDetection(object? sender, EventArgs e)
+    {
+        Console.CursorLeft = 7;
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write("Hostile Entity Identified");
+    }
+
+    /// <summary> Attempts to locate the CSV file relative to the working directory. </summary>
+    /// <returns> The relative path to the CSV file </returns>
+    /// <exception cref="System.IO.FileNotFoundException">Could not find radar-output.csv</exception>
     static string FindCSVPath()
     {
         if(File.Exists("radar-output.csv")) return "radar-output.csv";
