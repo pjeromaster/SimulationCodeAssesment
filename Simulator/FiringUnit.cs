71c36f021482fa367fe24682335b9bac1ae80985
Proxy <pjeromaster@gmail.com>
2024-06-13 12:20:05 +0200

    Removed seeded random

--- Simulator/FiringUnit.cs
+++ Simulator/FiringUnit.cs
@@ -1,10 +1,12 @@
-﻿using System.Diagnostics;
-
-namespace Simulator;
-
+﻿namespace Simulator;
+public interface IFiringUnit : IModel
+{
+    void FireMissile(object? sender, EventArgs e);
+    public event EventHandler<MissileFiredEventArgs>? OnMissileFired;
+}
 public class FiringUnit : IFiringUnit
 {
-    private readonly Random rnd;
+    private readonly Random rnd = new();
     private float killProbability;
     public float KillProbability
     {
@@ -13,28 +15,14 @@ public class FiringUnit : IFiringUnit
     }
 
     public event EventHandler<MissileFiredEventArgs>? OnMissileFired;
-    public FiringUnit(float killProbability) : this(killProbability, new Random()) { }
-    public FiringUnit(float killProbability, int seed) : this(killProbability, new Random(seed)) { }
-    public FiringUnit(float killProbability, Random rnd)
-    {
-        KillProbability = killProbability;
-        this.rnd = rnd;
-    }
+    public FiringUnit(float killProbability) => KillProbability = killProbability;
 
     public void FireMissile(object? sender, EventArgs e)
     {
-        float r = rnd.NextSingle();
-        Debug.WriteLine(r);
-
-        MissileFiredEventArgs eventArgs = new(r<= KillProbability);
+        MissileFiredEventArgs eventArgs = new(rnd.NextSingle()<= KillProbability);
         OnMissileFired?.Invoke(this, eventArgs);
     }
     public void Update() { }
-}
-public interface IFiringUnit : IModel
-{
-    void FireMissile(object? sender, EventArgs e);
-    public event EventHandler<MissileFiredEventArgs>? OnMissileFired;
 }
 
 public class MissileFiredEventArgs(bool targetNeutralized) : EventArgs
